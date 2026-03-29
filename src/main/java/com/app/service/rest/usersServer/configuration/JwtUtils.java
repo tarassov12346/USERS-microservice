132c686f719a40328c06432a2bd74ab0145580a4
Andrei Tarassov <tamogenia@gmail.com>
2026-03-29 12:37:42 +0300

    put secret to property

--- src/main/java/com/app/service/rest/usersServer/configuration/JwtUtils.java
+++ src/main/java/com/app/service/rest/usersServer/configuration/JwtUtils.java
@@ -3,19 +3,30 @@ package com.app.service.rest.usersServer.configuration;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 
 @Component
 public class JwtUtils {
-    // В реальном проекте вынеси это в application.properties
-    private final String jwtSecret = "mySecretKeyForTetrisProjectWhichMustBeAtLeast32CharactersLong!";
-    private final int jwtExpirationMs = 86400000; // 24 часа
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
-    private final SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    @Value("${jwt.expirationMs}")
+    private int jwtExpirationMs;
+
+    private SecretKey key; // Поле для хранения готового ключа
+
+    @PostConstruct
+    public void init() {
+        // Этот метод выполнится Спрингом сразу после заполнения полей
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String username, Long userId) {
         return Jwts.builder()
